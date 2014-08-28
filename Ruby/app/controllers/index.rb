16cbd2f8b1ff6b2a6157d94d9f529327fa077f42
Jaime & Amelia Laundy <pair+Jamanius.amelialaundy@devbootcamp.com>
2014-08-28 17:23:56 +1200

    completed sign in, create shoutout, save shout out, show my shout outs in theory

--- Ruby/app/controllers/index.rb
+++ Ruby/app/controllers/index.rb
@@ -12,8 +12,17 @@ post '/shout_out' do
 	if @user.is_password_valid(params[:password])
 		session[:user_id] = @user.id #session starts as an empty hash which is then assigned our users id
 		redirect '/shout_out'		#if the password is valid/matches, creates the session via a new key (rack magic), redirects to create_shout_out page
-	else	
+	else
 		@message = "login was invalid"
 		erb :sign_in
-	end	
+	end
 end
+
+post '/save_shout_out' do
+	ShoutOut.create(content: params[:new_shout_out_content], user_id: session[:user_id])
+	@all_shout_outs = ShoutOut.find_by_user_id(session[:user_id])
+	@user = User.find(session[:user_id])
+	erb :my_shout_outs
+end
+
+
