1b14e5f3b39c860fca4dff82b7fb9ce85e1a52fa
Jasmin Mayfield & Juliet Carpenter <pair+jasminmayfield.julietbc@devbootcamp.com>
2014-08-28 17:34:29 +1200

    Rakfile updated with test database capabilities and unit tests on Users added and working :)

--- Ruby/app/models/user.rb
+++ Ruby/app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
   has_many :shout_outs
-<<<<<<< HEAD
-=======
 
   def is_password_valid(submitted_password)
   	submitted_password == password
   end
->>>>>>> 90aa86f92ea4eff03996d6cebc8f2eea243e87eb
+
+  def self.validate(user_name,password)
+    where(handle: user_name, password: password).first
+  end
 end
+
