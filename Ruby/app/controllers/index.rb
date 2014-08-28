90aa86f92ea4eff03996d6cebc8f2eea243e87eb
Jasmin Mayfield <jasmin.mayfield@gmail.com>
2014-08-28 16:14:08 +1200

    created login session, changed HTML value

--- Ruby/app/controllers/index.rb
+++ Ruby/app/controllers/index.rb
@@ -1,8 +1,19 @@
 get '/' do
-  # Look in app/views/index.erb
-  erb :sign_in
+	erb :sign_in
 end
 
 get '/shout_out' do
-  erb :create_shout_out
+	@user = User.find(session[:user_id])
+	erb :create_shout_out
+end
+
+post '/shout_out' do
+	@user = User.find_by_handle(params[:user_name])
+	if @user.is_password_valid(params[:password])
+		session[:user_id] = @user.id #session starts as an empty hash which is then assigned our users id
+		redirect '/shout_out'		#if the password is valid/matches, creates the session via a new key (rack magic), redirects to create_shout_out page
+	else	
+		@message = "login was invalid"
+		erb :sign_in
+	end	
 end
--- Ruby/app/models/user.rb
+++ Ruby/app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_many :shout_outs
+
+  def is_password_valid(submitted_password)
+  	submitted_password == password
+  end
 end
