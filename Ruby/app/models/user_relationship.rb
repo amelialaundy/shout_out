2324b7224aa08676f0fe59a46f722e6588bfe572
Amelia Laundy <amelialaundy@gmail.com>
2014-08-29 12:18:08 +1200

    added follwers

--- Ruby/app/models/user_relationship.rb
+++ Ruby/app/models/user_relationship.rb
@@ -3,5 +3,6 @@ class UserRelationship < ActiveRecord::Base
   belongs_to :follower, class_name: 'User'
   belongs_to :followee, class_name: 'User'
   validates_uniqueness_of :follower_id, scope: :followee_id #make sure ever follower id is unique for each follow_id 
+  validates_uniqueness_of :followee_id, scope: :follower_id
 end
 
